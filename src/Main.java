e7483934db7066c8e7f7d586637450b99ec51d9e
Pawel <pawel.logo@gmail.com>
2025-03-14 18:47:50 +0100

    mod_1

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,31 @@
+import performance.DataGenerator;
+import performance.PerformanceTester;
+import sorting.BubbleSort;
+import sorting.MergeSort;
+import sorting.SortingAlgorithm;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        //just for testing
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Choose sorting algorithm: 1 - Bubble Sort");
+        System.out.println("Choose sorting algorithm: 2 - MergeSort");
+        int choice = scanner.nextInt();
+
+        SortingAlgorithm algorithm = null;
+        if (choice == 1) {
+            algorithm = new BubbleSort();
+        }
+        if (choice == 2) {
+            algorithm = new MergeSort();
+        }
+
+        int[] array = DataGenerator.generateRandomArray(1000);
+        long timeTaken = PerformanceTester.measureSortingTime(algorithm, array);
+
+        System.out.println("Sorting time = " + timeTaken + " ns");
     }
 }
