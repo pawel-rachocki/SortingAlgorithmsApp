6862e13bc979e1400ed749a3607364f4921b13e8
Pawel <pawel.logo@gmail.com>
2025-03-17 23:27:42 +0100

    Added QuickSort.java class

---
+++ src/sorting/QuickSort.java
@@ -0,0 +1,17 @@
+package sorting;
+
+public class QuickSort implements SortingAlgorithm{
+    public void sort(int[] arr){}
+
+    @Override
+    public String getName() {
+        return "QuickSort";
+    }
+
+    private void quickSort(int[] arr, int low, int high){
+
+    }
+    private int partition(int[] arr, int low, int high){
+        return 1;
+    }
+}
