4b64a790739708b305b1a8a0c333d460c0c6e084
Pawel <pawel.logo@gmail.com>
2025-03-18 18:54:44 +0100

    Added ArrayDisplay class for arrays visualisation

---
+++ src/gui/ArrayDisplay.java
@@ -0,0 +1,38 @@
+package gui;
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.canvas.Canvas;
+
+import javafx.scene.paint.Color;
+
+import java.awt.*;
+public class ArrayDisplay extends Canvas {
+    private int[] arr;
+    public ArrayDisplay(int width, int height){
+        super(width,height);
+    }
+
+    public void setArr(int[] array){
+        this.arr = array;
+        drawArray();
+    }
+
+
+    public void drawArray() {
+        if (arr == null) {
+            return;
+        }
+        GraphicsContext gc = getGraphicsContext2D();
+        gc.clearRect(0, 0, getWidth(), getHeight());
+
+        double barWidth = getWidth() / arr.length;
+        double maxHeight = getHeight();
+
+        for (int i = 0; i < arr.length; i++) {
+            double barHeight = (arr[i] / 1000.0) * maxHeight; // Skalowanie
+            gc.setFill(Color.BLUE);
+            gc.fillRect(i * barWidth, maxHeight - barHeight, barWidth - 2, barHeight);
+        }
+    }
+
+}
