81b626e818519c764e3cb195e77e01b4f4dcd1f3
Pawel <pawel.logo@gmail.com>
2025-03-19 00:05:00 +0100

    Added InsertionSort for testing

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
 import performance.DataGenerator;
 import performance.PerformanceTester;
-import sorting.BubbleSort;
-import sorting.MergeSort;
-import sorting.QuickSort;
-import sorting.SortingAlgorithm;
+import sorting.*;
 
 import java.util.Scanner;
 
@@ -15,6 +12,7 @@ public class Main {
         System.out.println("Choose sorting algorithm: 1 - Bubble Sort");
         System.out.println("Choose sorting algorithm: 2 - MergeSort");
         System.out.println("Choose sorting algorithm: 3 - QuickSort");
+        System.out.println("Choose sorting algorithm: 4 - InsertionSort");
         int choice = scanner.nextInt();
 
         SortingAlgorithm algorithm = null;
@@ -23,12 +21,13 @@ public class Main {
             case 1 -> algorithm = new BubbleSort();
             case 2 -> algorithm = new MergeSort();
             case 3 -> algorithm = new QuickSort();
+            case 4 -> algorithm = new InsertionSort();
             default -> {
                 break;
             }
         }
 
-        int[] array = DataGenerator.generateRandomArray(5);
+        int[] array = DataGenerator.generateRandomArray(5000);
         long timeTaken = PerformanceTester.measureSortingTime(algorithm, array);
 
         System.out.println("Sorting time = " + timeTaken + " ns");
