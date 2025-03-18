1b80fcc296d4ffd82fde1ad51824bfeacdd1803e
Pawel <pawel.logo@gmail.com>
2025-03-19 00:01:50 +0100

    Added InsertionSort.java class with implementation

---
+++ src/sorting/InsertionSort.java
@@ -0,0 +1,22 @@
+package sorting;
+
+public class InsertionSort implements SortingAlgorithm {
+    @Override
+    public void sort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+    @Override
+    public String getName() {
+        return "InsertionSort";
+    }
+}
