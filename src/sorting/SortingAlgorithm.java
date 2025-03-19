b076e1781a80c93670f126c56dcbed740128e3bd
Pawel <pawel.logo@gmail.com>
2025-03-19 19:01:07 +0100

    Changed sort method in the interface

--- src/sorting/SortingAlgorithm.java
+++ src/sorting/SortingAlgorithm.java
@@ -1,6 +1,8 @@
 package sorting;
 
+import java.util.function.Consumer;
+
 public interface SortingAlgorithm {
-    void sort(int[] arr);
+    void sort(int[] arr, Consumer<int[]> stepConsumer);
     String getName();
 }
